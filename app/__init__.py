a4f6e9b7d76d764284960ce00822f304db274fd7
Justen Morgan <justenamorgan@gmail.com>
2016-02-25 23:31:58 +0000

    update

--- app/__init__.py
+++ app/__init__.py
@@ -2,8 +2,8 @@ from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 
 app=Flask(__name__)
-
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://testuser:password@localhost/profiledb"
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+# app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://testuser:password@localhost/profiledb"
 
 db=SQLAlchemy(app)
 
