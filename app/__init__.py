2c2cc7aca0bbf272266b5d3f0f6c1625afa03e53
Justen Morgan <justenamorgan@gmail.com>
2016-02-26 00:11:04 +0000

    update

--- app/__init__.py
+++ app/__init__.py
@@ -3,8 +3,8 @@ from flask.ext.sqlalchemy import SQLAlchemy
 import os
 
 app=Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgres://qiuxgxiwedfala:iMUAzdx4siRlwJX0czJ7ti09EV@ec2-54-83-3-38.compute-1.amazonaws.com:5432/d97q1alb9u6ejs"
-# app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://testuser:password@localhost/profiledb"
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgres://xcismaysusivmw:l-ZuQFltgx54-zd5FMATK3uxRI@ec2-54-83-3-38.compute-1.amazonaws.com:5432/ddujna1vdv7ctd"
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://project1:project1@localhost/profilesdb"
 
 db=SQLAlchemy(app)
 
