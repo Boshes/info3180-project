8b5462de5d7f2b11f848acf2aaeeccdace96bdd4
Justen Morgan <justenamorgan@gmail.com>
2016-02-26 00:42:12 +0000

    update

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 import os
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.heroku import Heroku
 
 app=Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] =  'postgres://vcatibplosonrr:Gy8S844p-JG4HeBUsNNZ_wq_lo@ec2-54-83-3-38.compute-1.amazonaws.com:5432/d7lr2ugo0d839c'
